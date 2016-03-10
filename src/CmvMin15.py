f980e3672c898b7b58e4b11fc8378637415c06f6
Jayanth Mettu <jmettu@ooyala.com>
2016-03-10 12:16:54 -0800

    bug fixes in cmv min15

--- src/CmvMin15.py
+++ src/CmvMin15.py
@@ -6,12 +6,12 @@ from luigi.contrib.hdfs import HdfsTarget
 from CmvLib import *
 import json
 from datetime import timedelta
-import requests
+from pprint import pprint
 
 class InputSessionFile(luigi.ExternalTask):
     cube_time = luigi.DateMinuteParameter()
-    hdfs_sessions = luigi.Parameter()
-    hdfs_namenode = luigi.Parameter()
+    hdfs_sessions = luigi.Parameter(significant=False)
+    hdfs_namenode = luigi.Parameter(significant=False)
 
     def output(self):
         hdfs_str = self.cube_time.strftime(self.hdfs_sessions+'/%Y/%m/%d/%H/%M/_SUCCESS')
@@ -31,19 +31,20 @@ class CmvMin15(luigi.Task):
     sqlcmv_nameSpace = luigi.Parameter(significant=False)
     cassandra_seeds = luigi.Parameter(significant=False)
     jobserver_host = luigi.Parameter(significant=False)
-    hdfs_dir_list = []
-
-    tmpl_subst_params = {"start_time": start_time,
-                         "end_time": end_time,
-                         "key_space": key_space,
-                         "name_space": name_space,
-                         "cassandra_seeds": cassandra_seeds,
-                         "pcode_dic": prepare_ptz(get_providers_from_helios(), hdfs_dir_list)}
+    hdfs_sessions = luigi.Parameter(significant=False)
+    hdfs_dir_set = set()
 
     def process_config_tmpl(self, tmpl_file):
+        tmpl_subst_params = {"start_time": date_to_cmvformat(self.start_time),
+                             "end_time": date_to_cmvformat(self.end_time),
+                             "key_space": self.key_space,
+                             "name_space": self.name_space,
+                             "cassandra_seeds": self.cassandra_seeds,
+                             "pcode_dict": prepare_ptz(get_providers_from_helios(), list(self.hdfs_dir_set))}
+
         with open(tmpl_file) as json_file:
             json_data = json.load(json_file)
-            replace_config_params(json_data, self.tmpl_subst_params)
+            replace_config_params(json_data, tmpl_subst_params)
             return json_data
 
     def prepare_js_url(self):
@@ -55,25 +56,28 @@ class CmvMin15(luigi.Task):
     def requires(self):
         check_boundaries(self.start_time)
         check_boundaries(self.end_time)
-
-        cube_timeranges = []
+        cube_timeranges = set()
         now = self.start_time
         logging.info("end_time = %s", self.end_time)
         while now < self.end_time:
             logging.info("start_time = %s", self.start_time)
             logging.info("now_time = %s", now)
-            cube_timeranges.append(now)
-            self.hdfs_dir_list.append(self.cube_time.strftime(self.hdfs_sessions+'/%Y/%m/%d/%H/%M/'))
+            cube_timeranges.add(now)
+            self.hdfs_dir_set.add(now.strftime(self.hdfs_sessions+'/%Y/%m/%d/%H/%M/'))
             now += timedelta(minutes=15)
 
         return [InputSessionFile(cube_time=cube_time) for cube_time in cube_timeranges]
 
     def run(self):
         config_json = self.process_config_tmpl("/Users/jmettu/repos/analytics-workflow-service/utils/cmv_template.json")
-        submit_config_to_js(config_json, self.prepare_js_url())
+        with open('json.txt', 'w') as outfile:
+            json.dump(config_json, outfile, indent=4)
+        rslt_json = submit_config_to_js(config_json, self.prepare_js_url())
+        pprint(rslt_json)
 
     def output(self):
         return luigi.contrib.hdfs.HdfsTarget('/tmp/luigi-poc/touchme')
 
 if __name__ == '__main__':
-    luigi.run(['CmvMin15', '--workers', '1', '--local-scheduler'])
+    #luigi.run(['CmvMin15', '--workers', '1', '--local-scheduler'])
+    luigi.run(['CmvMin15', '--workers', '1'])
