ec82737593127951be17190821a547868c27ef39
Baohua Wang <baohua@ooyala.com>
2016-03-23 18:31:13 -0700

    Added weekly rollup.

---
+++ wario/rollup_weekly.py
@@ -0,0 +1,41 @@
+"""Rollup weekly task"""
+
+from datetime import timedelta
+
+import luigi
+
+from wario.rollup_base import CmvRollupBaseTask
+from wario.rollup_daily import CmvRollupDailyGenerator
+
+class CmvRollupWeeklyGenerator(CmvRollupBaseTask):
+    """Task for rollup weekly"""
+    wario_target_table_name = luigi.Parameter(default='rollup_week', significant=False)
+
+    def requires(self):
+        daily_rollups = []
+        day = self.get_start_time()
+        end_day = self.get_end_time()
+        while day != end_day:
+            daily_rollups.append(CmvRollupDailyGenerator(day=day, pcode=self.pcode, timezone=self.timezone))
+            day = day + timedelta(days=1)
+        return daily_rollups
+
+    def validate_day(self):
+        assert self.day.weekday() == 0
+
+    def get_start_time(self):
+        self.validate_day()
+        return self.day
+
+    def get_end_time(self):
+        self.validate_day()
+        return self.day + timedelta(days=7)
+
+    def get_rdd_duraion(self):
+        return 'day'
+
+    def get_rdd_rolled_duration(self):
+        return 'week'
+
+if __name__ == '__main__':
+    luigi.run(['CmvRollupWeeklyGenerator', '--workers', '1'])
