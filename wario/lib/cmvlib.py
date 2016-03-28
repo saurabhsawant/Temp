65d2b0d847b41a40524b62ed5317f91249599197
Jayanth Mettu <jmettu@ooyala.com>
2016-03-28 14:03:34 -0700

    bug fix in cmvlib

--- wario/lib/cmvlib.py
+++ wario/lib/cmvlib.py
@@ -206,7 +206,7 @@ class DateTime:
     def get_last_day_of_month(date):
         if date.month == 12:
             return date.replace(day=31)
-        return date.replace(month=date.month+1, day=1) - datetime.timedelta(days=1)
+        return date.replace(month=date.month+1, day=1) - timedelta(days=1)
 
     @staticmethod
     def utc_to_any_tz(utc_datetime, tz_iana_name):
