7cb61e17986987c1349f31ebb415d629903b574e
Jayanth Mettu <jmettu@ooyala.com>
2016-03-29 19:20:45 -0700

    add json files to package

--- setup.py
+++ setup.py
@@ -1,13 +1,15 @@
 __author__ = 'jmettu'
 
-import os
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='wario',
     version='1.0',
     description='workflow for IQ',
     url='http://github.services.ooyala.net/BigData/wario',
-    packages=['wario', 'wario.lib', 'wario.utils'],
+    packages=find_packages('wario'),
+    #package_dir={'wario': 'wario'},
+    package_data={'wario': 'wario/utils/*.json'},
+    include_package_data=True,
     entry_points={'console_scripts': ['wario=wario.cmdline:main']}
 )
