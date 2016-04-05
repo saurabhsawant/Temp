f08f86fbdd13759c57b1717400a3544eba3e6fc0
Jayanth Mettu <jmettu@ooyala.com>
2016-04-04 20:26:43 -0700

    bug fixes related to datadog

--- wario/rollup_base.py
+++ wario/rollup_base.py
@@ -110,7 +110,8 @@ class CmvRollupBaseTask(CmvBaseTask):
             logging.info("Rollup job completed successfully.")
         self.output().touch()
         #DataDogClient.gauge_this_metric(self.metric_name, time.time() - datadog_start_time, tags=self.tag_name)
-        statsd.histogram(self.metric_name,tags=self.tag_name)
+        statsd.histogram(self.metric_name, time.time()-datadog_start_time ,tags=self.tag_name)
+
 
     def output(self):
         self.metric_name, self.tag_name = self.rollup_datadog()
