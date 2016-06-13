a2cbbd9ab41256f2f4f4516f1b8ffa5c1c1fc304
Jayanth Mettu <jmettu@ooyala.com>
2016-06-12 23:39:35 -0700

    [1] added task for checking cube generation status [2] modified test script [3] added logging cfg to resources

--- test_with_cov.sh
+++ test_with_cov.sh
@@ -1,2 +1,4 @@
+#!/usr/bin/env bash
+export LUIGI_CONFIG_PATH=$PWD/test/resources/WarioTest.cfg
 coverage run -m unittest discover test -p *_test.py
 coverage report --include=*wario* --omit=*test*
---
+++ wario/check_cube_targets.py
@@ -0,0 +1,52 @@
+import luigi
+from datetime import timedelta
+from min15_generator import CmvMin15Generator
+from lib.cmvlib import CmvLib
+from rollup_daily import CmvRollupDailyGenerator
+from rollup_weekly import CmvRollupWeeklyGenerator
+
+
+class CmvCheckCubeTargets(luigi.Task):
+    start_time = luigi.DateMinuteParameter()
+    end_time = luigi.DateMinuteParameter()
+    task = luigi.Parameter()
+    target_root = luigi.Parameter(significant=False, default='/var/log/luigi_targets/checkcubes')
+
+    def requires(self):
+        pass
+
+    def run(self):
+        now = self.start_time
+        target_tasks = set()
+        if self.task == 'min15':
+            CmvLib.validate_min15_time(now)
+            while now < self.end_time:
+                ends_at = now + timedelta(minutes=15)
+                cmv_task = CmvMin15Generator(now, ends_at)
+                now = ends_at
+                target_tasks.add(cmv_task)
+        elif self.task == 'day':
+            while now < self.end_time:
+                ends_at = now + timedelta(days=1)
+                cmv_task = CmvRollupDailyGenerator(now, ends_at)
+                now = ends_at
+                target_tasks.add(cmv_task)
+        elif self.task == 'week':
+            now = now - timedelta(days=now.weekday)
+            while now < self.end_time:
+                ends_at = now + timedelta(weeks=1)
+                cmv_task = CmvRollupWeeklyGenerator(now, ends_at)
+                now = ends_at
+                target_tasks.add(cmv_task)
+        else:
+            raise ValueError("Incorrect task name provided. Should be one among min15, day, or week")
+
+        with self.output().open('w') as f:
+            for task in target_tasks:
+                if task.output().exists():
+                    f.write('%s,completed', task.start_time)
+                else:
+                    f.write('%s,not_completed', task.start_time)
+
+    def output(self):
+        return luigi.LocalTarget('%s/%s_%s_%s', self.target_root, self.start_time, self.end_time, self.task)
