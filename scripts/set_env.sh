0f802c70a9a19065af2efcc0ab032554dd9ab1a1
Jayanth Mettu <jmettu@ooyala.com>
2016-06-21 09:20:56 -0700

    [1]added session file resources [2] changes related to tests

--- scripts/set_env.sh
+++ scripts/set_env.sh
@@ -2,6 +2,7 @@
 luigi_cfg_file="$PWD/wario/resources/luigi.cfg"
 log_cfg_file="$PWD/wario/resources/log.cfg"
 log_file="$PWD/logs/wario.log"
+session_files="file://$PWD/test/resources/session_files"
 
 export LUIGI_CONFIG_PATH="$luigi_cfg_file"
 
@@ -11,6 +12,10 @@ fi
 
 if ! grep -q "$log_file" "$log_cfg_file"; then
     echo -e "\n[handler_logfile]\nargs:['$log_file', 'a']" >> $log_cfg_file
+fi
+
+if ! grep -q "$session_files" "$luigi_cfg_file"; then
+    echo -e "hdfs_sessions:$session_files"
 fi
 
 sudo cp $PWD/wario/resources/WarioCmv.cfg   /etc/luigi/WarioCmv.cfg
---
+++ test/min15_generator_test.py
@@ -0,0 +1,65 @@
+__author__ = 'jmettu'
+
+import unittest
+import luigi
+from helpers import LuigiTestCase
+from wario.min15_generator import CmvMin15Generator
+from datetime import datetime
+from luigi.file import LocalTarget
+
+import logging
+logger = logging.getLogger('luigi-interface')
+
+
+class Min15GeneratorTest(LuigiTestCase):
+
+    def setUp(self):
+        super(Min15GeneratorTest, self).setUp()
+        self.scheduler = luigi.scheduler.CentralPlannerScheduler(prune_on_get_work=False)
+        self.worker = luigi.worker.Worker(scheduler=self.scheduler)
+
+    def run_task(self, task):
+        self.worker.add(task)
+        self.worker.run()
+
+    def summary_dict(self):
+        return luigi.execution_summary._summary_dict(self.worker)
+
+    def summary(self):
+        return luigi.execution_summary.summary(self.worker)
+
+    def test_all_statuses(self):
+
+        class TestMin15Generator(luigi.Task):
+            start_time = datetime.strptime('2016-01-17T0045', '%Y-%m-%dT%H%M')
+            end_time = datetime.strptime('2016-01-17T0100', '%Y-%m-%dT%H%M')
+
+            def requires(self):
+                return CmvMin15Generator(self.start_time, self.end_time)
+
+            def run(self):
+                f = self.output().open('w')
+                f.write('exists')
+
+            def output(self):
+                return LocalTarget(path='test/resources/targets/TestMin15Generator.dep')
+
+        self.run_task(TestMin15Generator())
+        d = self.summary_dict()
+        print d
+
+
+if __name__ == '__main__':
+    unittest.main()
+
+
+
+
+
+
+
+
+
+
+
+
